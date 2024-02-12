a23d01007261b7a9f57d9c8525b40ec8cb9facc0
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-12 19:40:47 +0200

    create button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Button } from "./atoms/Button";
+
 const App = () => {
-  return <h1>hello world</h1>;
+  return <Button>HELLO</Button>;
 };
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  WHITE: "#FFF",
+  BLACK: "#000",
+  DARK_GRAY: "#1A1C1F",
+  MIDDLE_GRAY: "#23262A",
+  GRAY: "#353A3E",
+  LIGHT_GRAY: "#C1C1C1",
+};
+
+export const FONT_SIZE = {
+  BASE: "16px",
+  SMALL: "14px",
+  MIDDLE: "22px",
+  LARGE: "28px",
+  TITLE: "36px",
+};

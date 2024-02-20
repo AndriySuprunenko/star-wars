51032c3400d380d345e4203e356915a20444d33d
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-20 15:58:58 +0200

    add props validation

--- src/organisms/Loader.jsx
+++ src/organisms/Loader.jsx
@@ -1,17 +1,23 @@
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import PropTypes from 'prop-types';
 
 import { COLORS } from '../constants/constants';
 
-const Loader = () => {
+const Loader = ({ color }) => {
   return (
-    <Backdrop
-      sx={{ color: COLORS.WHITE, zIndex: 999 }}
-      open={true}
-    >
+    <Backdrop sx={{ color: color, zIndex: 999 }} open={true}>
       <CircularProgress color={COLORS.INHERIT} />
     </Backdrop>
   );
+};
+
+Loader.propTypes = {
+  color: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  color: COLORS.WHITE,
 };
 
 export default Loader;

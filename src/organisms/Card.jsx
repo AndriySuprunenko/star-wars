2d082672bbf7ccb9f2becac0296254b5b7b903c1
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-23 20:54:09 +0200

    fix

--- src/organisms/Card.jsx
+++ src/organisms/Card.jsx
@@ -4,7 +4,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-import Image from '../assets/reptile.jpeg';
+import Image from '../assets/luke.jpeg';
 
 export default function ActionAreaCard() {
   const openDrawer = () => {};
@@ -15,15 +15,21 @@ export default function ActionAreaCard() {
           component='img'
           height='140'
           image={Image}
-          alt='green iguana'
+          alt='Luke Skywalker'
         />
         <CardContent>
           <Typography gutterBottom variant='h5' component='div'>
-            Lizard
+            Luke Skywalker
           </Typography>
           <Typography variant='body2' color='text.secondary'>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            Luke Skywalker, a Force-sensitive human male, was a legendary Jedi
+            Master who fought in the Galactic Civil War during the reign of the
+            Galactic Empire. Along with his companions, Princess Leia Organa and
+            General Han Solo, Skywalker served as a revolutionary on the side of
+            the Alliance to Restore the Republic—an organization committed to
+            the downfall of the Galactic Empire and the restoration of
+            democracy. Following the war, Skywalker became a living legend, and
+            was remembered as one of the greatest Jedi in galactic history.
           </Typography>
         </CardContent>
       </CardActionArea>
